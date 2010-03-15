10fbd7c791635cb870b32057ce9395f5ee887e63
gnicolax <gnicolax@abcdf9d6-2776-11df-959e-df69dd781d82>
2010-03-15 16:26:44 +0000

    NV, Version 1.0 release snapshot

---
+++ tags/r1.0/org/gnvo/langpicker/LangPicker.java
@@ -0,0 +1,113 @@
+/*
+ *  This file is part of Language Picker Widget.
+ *
+ *  Language Picker Widget is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Language Picker Widget is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Language Picker Widget.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.gnvo.langpicker;
+
+
+import java.util.Locale;
+
+import android.app.ActivityManagerNative;
+import android.app.IActivityManager;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.RemoteException;
+//import android.util.Log;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+public class LangPicker extends AppWidgetProvider  {
+	private static final String LOG_TAG = "LangPicker";
+
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		Log.d(LOG_TAG, "onUpdate");
+
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+		for (int appWidgetId : appWidgetIds){
+			prepareAppWidget(context, appWidgetManager, appWidgetId);
+		}
+	}
+
+	static void prepareAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+		Log.d(LOG_TAG, "prepareAppWidget");
+		//TODO, create only one LangPickerDataAdapter object for insert(in the configure) and fetch
+		String language = new LangPickerDataAdapter().fetchWidgetLanguage(context, appWidgetId);
+
+		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+
+		Intent intent = new Intent(context, LocaleChangerReceiver.class);
+		intent.putExtra("language", language);
+		// We need to make the this intent unique:
+		intent.setData((Uri.parse("custom://langpicker/language/" + language)));
+
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+		views.setOnClickPendingIntent(R.id.language, pendingIntent);
+
+
+		views.setTextViewText(R.id.language, (new Locale(language).getDisplayLanguage()) + "\n" + language);
+
+		appWidgetManager.updateAppWidget(appWidgetId, views);
+	}
+
+	public void onReceive(Context context, Intent intent) {
+		final String action = intent.getAction();
+
+		if (AppWidgetManager.ACTION_APPWIDGET_DELETED.equals(action)) {
+			Bundle extras = intent.getExtras();
+			final int appWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+			if (appWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
+				this.onDeleted(context, new int[] { appWidgetId });
+			}
+		} else {
+			super.onReceive(context, intent);
+		}
+
+	} 
+
+	public static class LocaleChangerReceiver extends BroadcastReceiver {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			//TODO validate extras
+			Bundle extras = intent.getExtras();
+			String language = extras.getString("language");
+
+			try {
+				IActivityManager am = ActivityManagerNative.getDefault();
+				Configuration config = am.getConfiguration();
+
+				//TODO validate language
+				Locale loc = new Locale(language);
+				config.locale = loc;
+
+				config.userSetLocale = true;
+				am.updateConfiguration(config);
+
+			} catch (RemoteException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}   
+}
---
+++ tags/r1.0/org/gnvo/langpicker/WidgetConfigure.java
@@ -0,0 +1,154 @@
+/*
+ *  This file is part of Language Picker Widget.
+ *
+ *  Language Picker Widget is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Language Picker Widget is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Language Picker Widget.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.gnvo.langpicker;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ListActivity;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
+
+/**
+ * The configuration screen for the ExampleAppWidgetProvider widget sample.
+ */
+public class WidgetConfigure extends ListActivity{
+	private static final String LOG_TAG = "WidgetConfigure";
+	private static final int WARNING_DIALOG = 0;
+	private ListView mListView = null;
+	private int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		Log.d(LOG_TAG, "dialog id='" + id + "'");
+		switch (id) {
+		case WARNING_DIALOG:
+			Log.d(LOG_TAG, "inside warning dialog");			
+			return new AlertDialog.Builder(WidgetConfigure.this)
+			.setTitle(R.string.alert_dialog_language_not_selected)
+			.setPositiveButton(R.string.accept, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int whichButton) {
+				}
+			})
+			.create();
+		}
+		return null;
+	}
+
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setResult(RESULT_CANCELED);
+		setContentView(R.layout.widget_configure);
+
+		getAndValidateAppWidgetId();
+
+		populateList();
+
+		mListView = getListView();
+		mListView.setItemsCanFocus(false);
+		mListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+
+		Button newLocaleButton = (Button) findViewById(R.id.accept);
+
+		newLocaleButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				String language =  (String) mListView.getItemAtPosition(mListView.getCheckedItemPosition());
+
+				if (language != null){					
+					final Context context = v.getContext();
+					new LangPickerDataAdapter().addWidgetLanguage(context, mAppWidgetId, language.substring(language.lastIndexOf('-')+2));
+
+					AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+					LangPicker.prepareAppWidget(context, appWidgetManager, mAppWidgetId);
+
+					Intent resultValue = new Intent();
+					resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId);
+					setResult(RESULT_OK, resultValue);
+					finish();
+
+				} else {
+					showDialog(WARNING_DIALOG);
+				}
+
+				Log.d(LOG_TAG, "Selected text='" + language + "'");
+
+			}
+		});
+
+
+		registerForContextMenu(mListView);
+	}
+
+
+	/**
+	 * 
+	 */
+	private void getAndValidateAppWidgetId() {
+		Intent intent = getIntent();
+		Bundle extras = intent.getExtras();
+		if (extras != null) {
+			mAppWidgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+		}
+
+		// If they gave us an intent without the widget id, just bail.
+		if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+			finish();
+		}
+	}
+
+
+	/**
+	 * 
+	 */
+	private void populateList() {
+		ArrayList<String> data = new ArrayList<String>();
+
+		// Insert all system locales
+		String[] locales = getAssets().getLocales();
+		for (String locale : locales) {
+			if (locale != null && locale.length() > 2) {
+				Locale loc = new Locale(locale);
+				data.add(loc.getDisplayName() + " - " + locale);
+			}
+		}
+		locales = null;
+
+		// Sort all locales by language
+		Collections.sort(data);
+
+		setListAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_single_choice, data));
+
+	}
+}
